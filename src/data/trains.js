3f56f5c551cbc7a639c4501786a954fe7255e899
Artur <arturstud1nskyy@gmail.com>
2026-05-17 21:36:21 +0300

    створив файл trains.js де буде масив рейсів

---
+++ src/data/trains.js
@@ -0,0 +1,42 @@
+export const trains = [
+  {
+    id: 1,
+    number: "074O",
+    routeFrom: "Lviv",
+    routeTo: "Kyiv",
+    departureTime: "2026-05-20 07:15",
+    duration: "06:50"
+  },
+  {
+    id: 2,
+    number: "005K",
+    routeFrom: "Kyiv",
+    routeTo: "Kharkiv",
+    departureTime: "2026-05-20 09:20",
+    duration: "04:30"
+  },
+  {
+    id: 3,
+    number: "079P",
+    routeFrom: "Kyiv",
+    routeTo: "Dnipro",
+    departureTime: "2026-05-20 12:45",
+    duration: "05:10"
+  },
+  {
+    id: 4,
+    number: "012L",
+    routeFrom: "Kyiv",
+    routeTo: "Odesa",
+    departureTime: "2026-05-20 15:05",
+    duration: "07:25"
+  },
+  {
+    id: 5,
+    number: "045Z",
+    routeFrom: "Ivano-Frankivsk",
+    routeTo: "Lviv",
+    departureTime: "2026-05-20 18:30",
+    duration: "02:05"
+  }
+];
