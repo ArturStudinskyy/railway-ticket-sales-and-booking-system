71b635136041f53dfd616a617b505e46aee0fb88
Artur <arturstud1nskyy@gmail.com>
2026-05-17 22:17:46 +0300

    виправив інтерфейс

--- src/data/trains.js
+++ src/data/trains.js
@@ -2,41 +2,41 @@ export const trains = [
     {
         id: 1,
         number: "074O",
-        routeFrom: "Lviv",
-        routeTo: "Kyiv",
-        departureTime: "2026-05-20 07:15",
+        routeFrom: "Львів",
+        routeTo: "Київ-Пасажирський",
+        departureTime: "20 травня 2026, середа, 07:15",
         duration: "06:50"
     },
     {
         id: 2,
         number: "005K",
-        routeFrom: "Kyiv",
-        routeTo: "Kharkiv",
-        departureTime: "2026-05-20 09:20",
+        routeFrom: "Київ-Пасажирський",
+        routeTo: "Харків",
+        departureTime: "20 травня 2026, середа, 09:20",
         duration: "04:30"
     },
     {
         id: 3,
         number: "079P",
-        routeFrom: "Kyiv",
-        routeTo: "Dnipro",
-        departureTime: "2026-05-20 12:45",
+        routeFrom: "Київ-Пасажирський",
+        routeTo: "Дніпро",
+        departureTime: "20 травня 2026, середа, 12:45",
         duration: "05:10"
     },
     {
         id: 4,
         number: "012L",
-        routeFrom: "Kyiv",
-        routeTo: "Odesa",
-        departureTime: "2026-05-20 15:05",
+        routeFrom: "Київ-Пасажирський",
+        routeTo: "Одеса",
+        departureTime: "20 травня 2026, середа, 15:05",
         duration: "07:25"
     },
     {
         id: 5,
         number: "045Z",
-        routeFrom: "Ivano-Frankivsk",
-        routeTo: "Lviv",
-        departureTime: "2026-05-20 18:30",
+        routeFrom: "Івано-Франківськ",
+        routeTo: "Львів",
+        departureTime: "20 травня 2026, середа, 18:30",
         duration: "02:05"
     }
 ];
