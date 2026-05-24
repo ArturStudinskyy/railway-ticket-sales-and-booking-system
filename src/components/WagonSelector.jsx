6c942e2737df5d61c90a67eb279bea50f4bee307
Artur <arturstud1nskyy@gmail.com>
2026-05-24 16:15:57 +0300

    реалізував компоненту WagonSelector та логіку вибору вагона на сторінці бронювання

--- src/components/WagonSelector.jsx
+++ src/components/WagonSelector.jsx
@@ -1,5 +1,18 @@
-const WagonSelector = () => {
-    return <div>Вибір вагона</div>;
+const WagonSelector = ({ wagons, activeWagon, onSelectWagon }) => {
+    return (
+        <div>
+            {wagons.map((wagon) => (
+                <button
+                    key={wagon}
+                    type="button"
+                    onClick={() => onSelectWagon(wagon)}
+                    aria-pressed={wagon === activeWagon}
+                >
+                    {`Вагон ${wagon}`}
+                </button>
+            ))}
+        </div>
+    );
 };
 
 export default WagonSelector;
--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -1,7 +1,19 @@
+import { useState } from "react";
+
+import WagonSelector from "../components/WagonSelector";
+
 function BookingPage() {
+    const wagons = [1, 2, 3];
+    const [activeWagon, setActiveWagon] = useState(wagons[0]);
+
     return (
         <div>
             <h3>Booking Page</h3>
+            <WagonSelector
+                wagons={wagons}
+                activeWagon={activeWagon}
+                onSelectWagon={setActiveWagon}
+            />
         </div>
     );
 }
